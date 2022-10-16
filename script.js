2a7829ad9bd4b363255c2e97a6ffdbaa6959ccd5
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2022-10-16 20:29:43 -0300

    trazendo os áudios para constantes JavaScript, adicionando EventListener e criando as funções responsáveis por tocar o áudio e mudar o estilo do botão

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Botões das notas musicais
 const notaDo = document.querySelector('.nota-musical-do')
 const notaRe = document.querySelector('.nota-musical-re')
 const notaMi = document.querySelector('.nota-musical-mi')
@@ -5,3 +6,31 @@ const notaFa = document.querySelector('.nota-musical-fa')
 const notaSol = document.querySelector('.nota-musical-sol')
 const notaLa = document.querySelector('.nota-musical-la')
 const notaSi = document.querySelector('.nota-musical-si')
+
+// Áudios das notas musicais
+const audioDo = document.querySelector('.audio-do')
+const audioRe = document.querySelector('.audio-re')
+const audioMi = document.querySelector('.audio-mi')
+const audioFa = document.querySelector('.audio-fa')
+const audioSol = document.querySelector('.audio-sol')
+const audioLa = document.querySelector('.audio-la')
+const audioSi = document.querySelector('.audio-si')
+
+notaDo.addEventListener('click', () => tocarAudio(notaDo, audioDo));
+notaRe.addEventListener('click', () => tocarAudio(notaRe, audioRe))
+notaMi.addEventListener('click', () => tocarAudio(notaMi, audioMi))
+notaFa.addEventListener('click', () => tocarAudio(notaFa, audioFa))
+notaSol.addEventListener('click', () => tocarAudio(notaSol, audioSol))
+notaLa.addEventListener('click', () => tocarAudio(notaLa, audioLa))
+notaSi.addEventListener('click', () => tocarAudio(notaSi, audioSi))
+
+function buttonStyle (tecla) {
+  tecla.classList.remove('button-style-click')
+}
+
+function tocarAudio (tecla, audio) {
+  tecla.classList.add('button-style-click')
+  audio.play()
+  
+  setTimeout(() => buttonStyle(tecla), 900)
+}
